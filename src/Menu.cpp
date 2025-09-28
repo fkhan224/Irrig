be958ce9e32575c27611563069fb686eb5646f4d
chris-morgado <cmmorgado53@gmail.com>
2025-09-28 18:43:41 -0400

    PlatformIO setup

---
+++ src/Menu.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+char MoisHigh = 'L';
+char MoisSelec = 'M';
+int TMoisData[5] = {1, 2, 3, 4, 5};
+int AMoisData[5] = {4, 5, 6, 7, 8}; 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Moisture Highlighted: " + String(MoisHigh));
+  Serial.print(", Moisture Selected: " + String(MoisSelec));
+
+  Serial.print(", Target Data: ");
+  for (int i = 0; i < 5; i++) {
+  Serial.print(TMoisData[i]); // Print the current element
+    if (i < 4) {
+      Serial.print(", "); // Print a comma and space between elements (except for the last one)
+    }
+  }
+  
+  Serial.print(", Actual Data: ");
+  for (int i = 0; i < 5; i++) {
+  Serial.print(AMoisData[i]); // Print the current element
+    if (i < 4) {
+      Serial.print(", "); // Print a comma and space between elements (except for the last one)
+    }
+  }
+  Serial.println();
+}
